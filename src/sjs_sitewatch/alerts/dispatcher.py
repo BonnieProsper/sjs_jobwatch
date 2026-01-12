cb9bfc22de791c65139752073759be930380182f
BonnieProsper <bonniep.mcconnell@gmail.com>
2026-01-12 16:37:37 +1300

    update alerts pipeline

--- src/sjs_sitewatch/alerts/dispatcher.py
+++ src/sjs_sitewatch/alerts/dispatcher.py
@@ -1,4 +1,4 @@
-# TODO: delete dispatcher but its used in pipeline?
+# TODO: rename to alerts/scorer.py, AlertScorer, dispatch = score_all()
 from typing import Iterable, List
 
 from sjs_sitewatch.alerts.models import ScoredChange
